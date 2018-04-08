4a7343ba150796c24a5abd95e8713d00c034f935
Dega96 <38177807+Dega96@users.noreply.github.com>
2018-04-08 12:46:28 +0200

    Update fulladd_8_bit_map.vhd

--- fulladd_8_bit_map.vhd
+++ fulladd_8_bit_map.vhd
@@ -62,7 +62,7 @@ c(0) <= '0';  --il primo carry inizializzato a 0
 --end generate;
 --stage8 : fulladd port map (Cin => c(7), Cout => Cout, a => A(7), b => B(7), s => S(7));
 
-G: FOR i IN 0 to N-1 generate --ciclo generate
+FA0: FOR i IN 0 to N-1 generate --ciclo generate
       stage_i: fulladd PORT MAP (Cin=>c(i), a=>A(i), b=>B(i), sum=>SUM(i), Cout=>c(i+1));
    end generate;
 -- fine blocco full add
