a09d1448a14c09ac6bc7b9d730456937249e8de9
Dega96 <38177807+Dega96@users.noreply.github.com>
2018-04-08 19:00:58 +0200

    Add files via upload

---
+++ convert_pos_neg.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity convert_pos_neg is 
+	generic ( N : integer:=8);
+	
+	port
+		(
+			B		:IN signed(N-1 downto 0);
+			B_neg				:Out signed(N-1 downto 0)
+		);
+	end convert_pos_neg;
+	
+architecture behavior of convert_pos_neg is
+	signal temp : signed(N-1 downto 0);
+		
+		begin
+			temp <= not B;
+			B_neg   <= temp + 1;
+		
+end behavior;
