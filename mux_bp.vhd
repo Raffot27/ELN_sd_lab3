088b8a48be670dc72dd22a37b8eb7c0dee048989
Raffot27 <37403051+Raffot27@users.noreply.github.com>
2018-04-08 22:11:57 +0200

    Add files via upload

---
+++ mux_bp.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux_bp is 
+port ( a, b	:IN std_logic;
+       skip  :IN std_logic;
+       Cout			:OUT std_logic );
+		 
+end mux_bp;
+
+architecture stru of mux_bp is
+
+begin
+  Cout<=( a and not skip) or (b and  skip);
+   
+end stru;
+       
+        
