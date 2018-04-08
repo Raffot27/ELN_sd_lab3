3e43da74763462f7c630f6b507381fe9ff2670df
Raffot27 <37403051+Raffot27@users.noreply.github.com>
2018-04-08 10:22:13 +0200

    Add files via upload

---
+++ Full_adder.vhd
@@ -0,0 +1,18 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+entity fulladd is 
+port (
+		Cin, a, b 	:in std_logic;
+	   sum, Cout	:out std_logic
+	   );
+end fulladd;
+
+architecture structure of fulladd is
+signal sel		:std_logic;
+begin
+   sel<= a xor b;
+	sum<= Cin xor sel;
+	Cout<= (sel and Cin) or ( not(sel) and b);
+end structure;	
